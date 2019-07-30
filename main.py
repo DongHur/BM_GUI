b48efef4599929781575279f0f258c52992a692b
Dong Hur <dhur@college.harvard.edu>
2019-07-29 21:42:46 -0400

    testing branch

--- main.py
+++ main.py
@@ -21,6 +21,8 @@ from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as NavigationToolbar
 from matplotlib.figure import Figure
 
+# test
+
 class Worker(QObject):
     play = pyqtSignal()
     pause = pyqtSignal()
